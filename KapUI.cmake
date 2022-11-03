3dc78030ea37e0061ea8324a021cc6d5b0f74b9e
benji-35 <benjamin.delvert@epitech.eu>
2022-11-03 13:40:31 +0100

    ADD : beta version

--- KapUI.cmake
+++ KapUI.cmake
@@ -1,1 +1,12 @@
+
+option(KAPUI_BETA_OPT "Active or not beta version of KapUI" OFF)
+
+if (${KAPUI_BETA_OPT})
+    set(KAPUI_BETA_FLAG "-DKAPUI_BETA_OW=true")
+else()
+    set(KAPUI_BETA_FLAG "-DKAPUI_BETA_OW=true")
+endif()
+
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${KAPUI_BETA_FLAG}")
+
 include_directories(Packages/KapUI)
--- KapUI/KapUI.hpp
+++ KapUI/KapUI.hpp
@@ -8,6 +8,19 @@
 #ifndef KAPUI_HPP_
 #define KAPUI_HPP_
 
+#ifdef KAPUI_BETA_OW
+    #ifndef KAPUI_BETA
+        #define KAPUI_BETA KAPUI_BETA_OW
+    #else
+        #undef KAPUI_BETA
+        #define KAPUI_BETA KAPUI_BETA_OW
+    #endif
+#else
+    #ifndef KAPUI_BETA
+        #define KAPUI_BETA false
+    #endif
+#endif
+
 #define KAPUI_VERSION_MAJOR 0
 #define KAPUI_VERSION_MINOR 102
 #define KAPUI_VERSION "0.102"
